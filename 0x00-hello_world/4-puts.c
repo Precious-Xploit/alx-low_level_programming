f5182a6c8a144e84580fd5b8cd400f9d3260aeae
Precious-Xploit <preciouxxploit@gmail.com>
2022-09-08 10:24:23 -0700

    Task four

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+  * main-Entry point
+  * Return: 0 if successful
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

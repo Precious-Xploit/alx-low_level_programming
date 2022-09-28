b72ba32bc6e632b246b88f9756af7d7980ad308a
Precious-Xploit <preciouxxploit@gmail.com>
2022-09-28 02:19:20 -0700

    Task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y.
- * @x: base.
- * @y: exponent.
- * Return: value of the exponentiation.
+ * factorial - returns the factorial of a given number.
+ * @n: input number.
+ * Return: factorial of the number.
  */
-int _pow_recursion(int x, int y)
+int factorial(int n)
 {
-	if (y < 0)
+	if (n < 0)
 		return (-1);
-	else if (y == 0)
+	else if (n == 0)
 		return (1);
 	else
-		return (x * _pow_recursion(x, y - 1));
+		return (n * factorial(n - 1));
 }

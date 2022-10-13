9692cb3f40cb1b5d3838eacfae1c7279c171398c
Precious-Xploit <preciouxxploit@gmail.com>
2022-10-13 02:36:59 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,63 +1,57 @@
-#include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
- * op_add - addition of two intergers
- * @a: first interger
- * @b: second interger
- * Return: sum of intergers
- */
+ * op_add - Adds two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
 int op_add(int a, int b)
 {
 return (a + b);
 }
+
 /**
- * op_sub - subtracting  two intergers
- * @a: first interger
- * @b: second interger
- * Return: product of operation
- */
+ * op_sub - subtracts two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
+
 /**
- * op_div -division  two intergers
- * @a: first interger
- * @b: second interger
- * Return: product of operation
- */
-int op_div(int a, int b)
-{
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
-}
-return (a / b);
-}
-/**
- * op_mul - multiplying  two intergers
- * @a: first interger
- * @b: second interger
- * Return: product of operation
- */
+ * op_mul - Multiplies two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
+
 int op_mul(int a, int b)
 {
 return (a * b);
 }
+
 /**
- * op_mod -remainderof the division of two intergers
- * @a: first interger
- * @b: second interger
- * Return: product of operation
- */
+ * op_div - Divides two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modules two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
+
 int op_mod(int a, int b)
 {
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
-}
 return (a % b);
 }
